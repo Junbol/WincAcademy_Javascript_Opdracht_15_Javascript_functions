99024cbef1486a4f370388a93bff5a2094e16d98
Junier <junierba@yahoo.co.uk>
2021-11-07 23:29:32 +0100

    Add files via upload

---
+++ Opdracht_15_Javascript_functions/javascript_functions.js
@@ -0,0 +1,70 @@
+console.log("Hello Winc Academy students");
+
+/* let Ingredients = [
+  beef_meat,
+  pork_meat,
+  onion,
+  garlic,
+  vegetables,
+  oil,
+  spaguetti,
+]; */
+
+const makeSpaghettiBolognese = function () {
+  console.log("RECIPE: SpaghettiBolognese");
+  console.log("1. Boil water");
+  console.log("2. Cut vegetables");
+  console.log(
+    "3. Throw oil, 2 onions, 2 garlic heads and 3 tomatoes on a hot pan"
+  );
+  console.log("4. Put the half and half chopped and let it cook for 5 min ");
+  console.log("5. Put the spaguetti on the boiling water for 10 minutes");
+  console.log(
+    "6. Add more tomatoes, the vegetables and water to make the sauce, cook for 5 min"
+  );
+  console.log(
+    "7. Take the spaguetti out of the pan and let it the water drain"
+  );
+  console.log(
+    "8. Add ready made tomato sauce to the pan with the meat and vegetables, cook it for 5 min more"
+  );
+  console.log(
+    "9. Put the spaghetti on a dish and throw some grated parmmesan cheese on top "
+  );
+  console.log("Buon appetito!");
+};
+
+makeSpaghettiBolognese();
+
+const makeSushi = function () {
+  console.log("RECIPE: Sushi");
+  console.log("1. Prep all of your ingredients.");
+  console.log("2. Press the rice. ");
+  console.log("3. Add the fillings.");
+  console.log("4. Roll up the sushi.");
+  console.log("5. Cut the sushi.");
+  console.log("6. Drizzle and garnish.");
+  console.log("Dōzo omeshiagarikudasai!");
+};
+
+const makePizza = function () {
+  console.log("Sorry we dont have Pizza today");
+};
+
+makeSpaghettiBolognese();
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
+
+/* 
+//CREATING OBJECTS PART 1 AND 2
+
+let person = {
+  name: "Junier",
+  age: 55,
+};
+ */
